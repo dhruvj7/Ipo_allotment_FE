d66a326902e8bb02323a551d9e469feb378e8ce6
dhruvj7 <dhruvjain076@outlook.com>
2025-10-30 23:28:58 +0530

    redesign and move to signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Home } from "./home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsModule, Home],
+  imports: [Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
