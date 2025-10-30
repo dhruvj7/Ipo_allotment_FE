38060b926ccc43ca196baddbb5c5e805f8f4eeff
dhruvj7 <dhruvjain076@outlook.com>
2025-10-30 18:56:41 +0530

    table for results created

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -4,18 +4,32 @@ import { ReactiveFormsModule,FormsModule, FormBuilder, FormGroup, Validators } f
 import { IpoService } from '../services/ipo.service';
 import { IPOINTERFACE } from '../Interfaces/ipoInterface';
 import { ChangeDetectorRef } from '@angular/core';
+import { ALLOTMENTREQUEST } from '../Interfaces/allotmentRequest';
+import { ALLOTMENTRESULT } from '../Interfaces/allotmentResult';
 
 @Component({
   selector: 'app-home',
-  imports: [ReactiveFormsModule,FormsModule,JsonPipe,CommonModule],
+  imports: [ReactiveFormsModule,FormsModule,CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
   ipoForm: FormGroup;
   ipoList : IPOINTERFACE[] = [];
+  selectedIpo : any;
+  resultDisplay : Boolean = false;
 
-  constructor(private fb:FormBuilder, private ipoService : IpoService, private cdf : ChangeDetectorRef){
+
+  allotmentResult:ALLOTMENTRESULT = {
+    message : "",
+    panNumber : "",
+    applicantName : "",
+    numberOfSharesAlloted : "",
+    numberOfSharesApplied : "",
+    allotted : false
+  };
+
+  constructor(private fb:FormBuilder, private ipoService : IpoService, private cdr : ChangeDetectorRef){
     this.ipoForm = this.fb.group({
       ipoName : ["",Validators.required],
       panNumber : ["",Validators.required]
@@ -23,12 +37,10 @@ export class Home {
   }
 
   ngOnInit(){
-
     this.ipoService.getAvailableIpos().subscribe({
       next : (ipoList : IPOINTERFACE [])=>{
         this.ipoList = ipoList;
-        this.cdf.detectChanges();
-        console.log(this.ipoList)
+        this.cdr.detectChanges();
       },
       error : (err)=>{
         console.log(err)
@@ -38,7 +50,40 @@ export class Home {
     
   }
 
-  getAllotmentDetails(){
+  getAllotmentDetails() {
+    const selectedCompanyId = this.ipoForm.value.ipoName;
+    const panNumber = this.ipoForm.value.panNumber;
 
+    this.selectedIpo = this.ipoList.find(
+      (ipo) => ipo.companyId === selectedCompanyId
+    );
+
+    if (!this.selectedIpo) {
+      console.warn('No IPO found for selected companyId');
+      return;
+    }
+
+    const payload: ALLOTMENTREQUEST = {
+      clientid: this.selectedIpo.companyId,
+      PAN: panNumber,
+      IFSC: "",
+      CHKVAL: "1",
+      token: "" // Replace with UUID or your token logic
+    };
+
+
+    this.ipoService.getAllotmentDetails(payload,this.selectedIpo.registrarId).subscribe({
+      next : (res)=>{
+        this.allotmentResult=res;
+        this.resultDisplay = true;
+        this.cdr.detectChanges();
+      },
+      error : (err)=>{}
+    })
+  }
+
+  back(){
+    this.resultDisplay= false;
+    this.cdr.detectChanges();
   }
 }
--- src/app/services/ipo.service.ts
+++ src/app/services/ipo.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environment/environment';
 import { IPOINTERFACE } from '../Interfaces/ipoInterface';
-import { BehaviorSubject } from 'rxjs';
+import { ALLOTMENTREQUEST } from '../Interfaces/allotmentRequest';
+import { ALLOTMENTRESULT } from '../Interfaces/allotmentResult';
 
 @Injectable({
   providedIn: 'root',
@@ -14,4 +15,9 @@ export class IpoService {
   getAvailableIpos(){
     return this.http.get<IPOINTERFACE []> (environment.BASE_URL + "/ipos");
   }
+
+  getAllotmentDetails(allotmentPayload : ALLOTMENTREQUEST, registrarId: Number){
+    return this.http.post<ALLOTMENTRESULT>(environment.BASE_URL+`/ipo/checkAllotment/${registrarId}`, allotmentPayload);
+  }
+
 }
